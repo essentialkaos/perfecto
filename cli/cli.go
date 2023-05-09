44ed6a7388798b98d303b1bad527ad22cd32c34c
Anton Novojilov <andy@essentialkaos.com>
2023-05-10 01:35:53 +0300

    Custom version info formats support

--- cli/cli.go
+++ cli/cli.go
@@ -89,7 +89,7 @@ var optMap = options.Map{
 	OPT_NO_LINT:     {Type: options.BOOL},
 	OPT_NO_COLOR:    {Type: options.BOOL},
 	OPT_HELP:        {Type: options.BOOL},
-	OPT_VER:         {Type: options.BOOL},
+	OPT_VER:         {Type: options.MIXED},
 
 	OPT_VERB_VER:     {Type: options.BOOL},
 	OPT_COMPLETION:   {},
@@ -130,7 +130,7 @@ func Run(gitRev string, gomod []byte) {
 		printMan()
 		os.Exit(0)
 	case options.GetB(OPT_VER):
-		genAbout(gitRev).Print()
+		genAbout(gitRev).Print(options.GetS(OPT_VER))
 		os.Exit(0)
 	case options.GetB(OPT_VERB_VER):
 		support.Print(APP, VER, gitRev, gomod)
