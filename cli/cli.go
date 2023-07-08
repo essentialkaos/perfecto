c566a8b59425ccf087917bc4289b874ac56f2100
Anton Novojilov <andy@essentialkaos.com>
2023-07-09 01:47:19 +0300

    Fix printing help content if no specs provided

--- cli/cli.go
+++ cli/cli.go
@@ -135,7 +135,7 @@ func Run(gitRev string, gomod []byte) {
 	case options.GetB(OPT_VERB_VER):
 		support.Print(APP, VER, gitRev, gomod)
 		os.Exit(0)
-	case options.GetB(OPT_HELP) || len(args) == 2:
+	case options.GetB(OPT_HELP) || len(args) == 0:
 		genUsage().Print()
 		os.Exit(0)
 	}
