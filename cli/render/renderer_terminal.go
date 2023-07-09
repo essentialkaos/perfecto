032b86d1ddfed8c8f8a533711bfde2a8c66cecec
Anton Novojilov <andy@essentialkaos.com>
2023-07-09 22:25:00 +0300

    Improve terminal render

--- cli/render/renderer_terminal.go
+++ cli/render/renderer_terminal.go
@@ -190,7 +190,7 @@ func (r *TerminalRenderer) renderShortReport(report *check.Report) {
 func (r *TerminalRenderer) renderTinyReport(file string, report *check.Report) {
 	specName := strutil.Exclude(path.Base(file), ".spec")
 
-	fmtc.Printf("%24s.spec: ", specName)
+	fmtc.Printf("%24s: ", specName)
 
 	categories := map[uint8][]check.Alert{
 		check.LEVEL_NOTICE:   report.Notices,
